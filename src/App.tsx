609c5fb779f363592c92012f853fb32c81d80649
Ilya Ozerov <efwe456lpzx>
2021-10-25 14:47:43 +0300

    Added full html template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,296 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+
+      <section className="home">
+
+        <div className="home__content">
+          <header className="home__header header">
+            <div className="header__logo logo">
+              <img src="#" alt="krean logo" />
+            </div>
+            <ul className="header__list">
+              <li>Services</li>
+              <li>Works</li>
+              <li>References</li>
+              <li>Contact</li>
+            </ul>
+          </header>
+
+          <div className="home__title">
+
+            <h1>Digital Marketing</h1>
+
+            <div className="home__subtitle">
+              <h2>Consultancy</h2>
+            </div>
+
+          </div>
+
+          <div className="home__mouse">
+            <img src="#" alt="computer mouse" />
+          </div>
+
+        </div>
+
+      </section>
+
+      <section className="services">
+        <div className="services__content">
+          <div className="services__logo">
+            KREA.
+          </div>
+
+          <div className="services__title title">
+            <h2>Services</h2>
+          </div>
+
+          <div className="services__columns">
+
+            <div className="services__column column">
+              <div className="column__image">
+                <img src="#" alt="column" />
+              </div>
+              <div className="column__title">Consultancy</div>
+              <div className="column__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget elementum nisi. Curabitur cursus, dolor ac tempus pulvinar, risus risus elementum eros, vehicula ultricies quam arcu at augue. Donec lacinia erat nec tincidunt dignissim. Etiam egestas euismod ipsum, vitae commodo neque lacinia ac
+              </div>
+            </div>
+
+            <div className="services__column column">
+              <div className="column__image">
+                <img src="#" alt="column" />
+              </div>
+              <div className="column__title">Mentorship</div>
+              <div className="column__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget elementum nisi. Curabitur cursus, dolor ac tempus pulvinar, risus risus elementum eros, vehicula ultricies quam arcu at augue. Donec lacinia erat nec tincidunt dignissim. Etiam egestas euismod ipsum, vitae commodo neque lacinia ac
+              </div>
+            </div>
+
+            <div className="services__column column">
+              <div className="column__image">
+                <img src="#" alt="column" />
+              </div>
+              <div className="column__title">Incubatory</div>
+              <div className="column__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget elementum nisi. Curabitur cursus, dolor ac tempus pulvinar, risus risus elementum eros, vehicula ultricies quam arcu at augue. Donec lacinia erat nec tincidunt dignissim. Etiam egestas euismod ipsum, vitae commodo neque lacinia ac
+              </div>
+            </div>
+
+          </div>
+
+          <div className="services__button">
+            <button>GET IN TOUCH</button>
+          </div>
+        </div>
+      </section>
+
+      <section className="works">
+        <div className="works__title title">
+          <h2>Works</h2>
+        </div>
+
+        <div className="works__items">
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+          <div className="works__item">
+            <img src="#" alt="work item" />
+          </div>
+
+        </div>
+
+
+      </section>
+
+      <section className="references">
+
+        <div className="references__content">
+          <div className="references__title">
+            <h2>References</h2>
+          </div>
+
+          <div className="references__comments comments">
+
+            <div className="comments__item">
+              <div className="comments__image">
+                <img src="#" alt="author" />
+              </div>
+              <div className="comments__body">
+                <div className="comments__name">John Doe</div>
+                <div className="comments__text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget elementum nisi. Curabitur cursus, dolor ac tempus pulvinar, risus risus elementum eros, vehicula ultricies quam arcu at augue. Donec lacinia erat nec tincidunt dignissim. Etiam egestas euismod ipsum, vitae commodo neque lacinia ac
+                </div>
+              </div>
+            </div>
+
+            <div className="comments__item">
+              <div className="comments__image">
+                <img src="#" alt="author" />
+              </div>
+              <div className="comments__body">
+                <div className="comments__name">John Doe</div>
+                <div className="comments__text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget elementum nisi. Curabitur cursus, dolor ac tempus pulvinar, risus risus elementum eros, vehicula ultricies quam arcu at augue. Donec lacinia erat nec tincidunt dignissim. Etiam egestas euismod ipsum, vitae commodo neque lacinia ac
+                </div>
+              </div>
+            </div>
+
+            <div className="comments__item">
+              <div className="comments__image">
+                <img src="#" alt="author" />
+              </div>
+              <div className="comments__body">
+                <div className="comments__name">John Doe</div>
+                <div className="comments__text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget elementum nisi. Curabitur cursus, dolor ac tempus pulvinar, risus risus elementum eros, vehicula ultricies quam arcu at augue. Donec lacinia erat nec tincidunt dignissim. Etiam egestas euismod ipsum, vitae commodo neque lacinia ac
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className="references__partners partners">
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+
+            <div className="partners__item">
+              <div className="partners__image">
+                <img src="#" alt="patner" />
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      <section className="contact">
+        <div className="contact__content">
+          <div className="contact__form">
+            <div className="contact__name">
+              <input type="text" placeholder="Full Name" />
+            </div>
+            <div className="contact__email">
+              <input type="text" placeholder="Email" />
+            </div>
+            <div className="contact__message">
+              <textarea placeholder="Message" />
+            </div>
+            <div className="contact__button">
+              <button>Send</button>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+      <footer className="footer">
+        <div className="footer__content">
+          <div className="footer__logo logo">KREA.</div>
+          <ul className="logo__address">
+            <li>John Street,</li>
+            <li>Doe Building</li>
+            <li>11/8   EDIRNE/TURKEY</li>
+          </ul>
+
+          <div className="logo__contacts">
+            <div className="footer__email">info@envato.com</div>
+            <div className="footer__number">900 800 70 60</div>
+          </div>
+
+          <div className="footer__socials">
+            <div className="footer__networks">
+              <div className="footer__network">
+                <img src="#" alt="facebook icon" />
+              </div>
+
+              <div className="footer__network">
+                <img src="#" alt="twitter icon" />
+              </div>
+
+              <div className="footer__network">
+                <img src="#" alt="linkedin icon" />
+              </div>
+
+              <div className="footer__network">
+                <img src="#" alt="google icon" />
+              </div>
+            </div>
+            <div className="footer__copyright">All rights reserved.</div>
+          </div>
+
+        </div>
+
+      </footer>
+
     </div>
   );
 }
-
-export default App;
